c143598b167a144717d0aac1d210fa16ace53850
AMERHAJIJ <amerhajij81@gmail.com>
2025-07-27 23:43:29 +0300

    all categroy add with out seen vidoes and subject tetchers

--- database/migrations/2025_07_27_190458_create_videos_table.php
+++ database/migrations/2025_07_27_190458_create_videos_table.php
@@ -11,17 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('videos', function (Blueprint $table) {
             $table->id();
-           $table->string('title');
-            $table->text('content');
+            $table->string('name');
             $table->string('image')->nullable();
+            $table->string('link');
             $table->unsignedBigInteger('visits')->default(0);
+            $table->boolean('isRequired')->default(false);
             $table->boolean('showInHomePage')->default(false);
             $table->boolean('active')->default(true);
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('Category')->onDelete('cascade');
+            $table->string('object_type'); // category or subject
+            $table->unsignedBigInteger('object_id');
             $table->timestamps();
+
+            // Optional: You can index polymorphic relation for performance
+            $table->index(['object_type', 'object_id']);
         });
     }
 
@@ -30,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blogs');
+        Schema::dropIfExists('videos');
     }
 };
